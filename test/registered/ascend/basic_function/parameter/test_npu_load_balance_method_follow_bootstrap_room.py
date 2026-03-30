3505ce1943063e9a54966295e1c50f34ba3fa040
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-03-30 16:07:37 +0800

    debug

--- test/registered/ascend/basic_function/parameter/test_npu_load_balance_method_follow_bootstrap_room.py
+++ test/registered/ascend/basic_function/parameter/test_npu_load_balance_method_follow_bootstrap_room.py
@@ -20,13 +20,17 @@ from sglang.test.test_utils import (
 
 register_npu_ci(est_time=200, suite="nightly-4-npu-a3", nightly=True)
 
+load_balance_method_options = ["auto", "round_robin", "total_requests", "total_tokens", "follow_bootstrap_room"]
+all_params = list(itertools.product(load_balance_method_options, repeat=2))
 
-class TestNPULoadBalanceMethodDPDisaggregation(TestDisaggregationBase):
+class BaseTestNPULoadBalanceMethodDPDisaggregation(TestDisaggregationBase):
     """Testcase：Verify that the inference is successful when --load-balance-method is set to follow_bootstrap_room.
 
     [Test Category] Parameter
     [Test Target] --load-balance-method
     """
+
+    params = None
 
     # load_balance_method = "follow_bootstrap_room"
     # load_balance_method = "round_robin"
@@ -36,86 +40,23 @@ class TestNPULoadBalanceMethodDPDisaggregation(TestDisaggregationBase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.prefill_load_balance_method, cls.decode_load_balance_method = cls.params
         cls.model = QWEN3_30B_A3B_INSTRUCT_2507_WEIGHTS_PATH
         os.environ["ASCEND_MF_STORE_URL"] = "tcp://127.0.0.1:24666"
 
         # Non blocking start servers
-        # cls.start_prefill()
-        # cls.start_decode()
-        #
-        # # Block until both
-        # cls.wait_server_ready(cls.prefill_url + "/health")
-        # cls.wait_server_ready(cls.decode_url + "/health")
-        #
-        # cls.launch_lb()
+        cls.start_prefill()
+        cls.start_decode()
+
+        # Block until both
+        cls.wait_server_ready(cls.prefill_url + "/health")
+        cls.wait_server_ready(cls.decode_url + "/health")
+
+        cls.launch_lb()
         cls.url = urlparse(cls.lb_url)
 
-    # @classmethod
-    # def start_prefill(cls):
-    #     prefill_args = [
-    #         "--disaggregation-mode",
-    #         "prefill",
-    #         "--tp-size",
-    #         "2",
-    #         "--enable-dp-attention",
-    #         "--dp",
-    #         "2",
-    #         "--load-balance-method",
-    #         cls.prefill_load_balance_method,
-    #         "--disaggregation-transfer-backend",
-    #         "ascend",
-    #         "--disable-cuda-graph",
-    #         "--attention-backend",
-    #         "ascend",
-    #         "--mem-fraction-static",
-    #         0.8,
-    #         "--dist-init-addr",
-    #         "127.0.0.1:10100",
-    #         "--base-gpu-id",
-    #         4,
-    #     ]
-    #
-    #     cls.process_prefill = popen_launch_pd_server(
-    #         cls.model,
-    #         cls.prefill_url,
-    #         timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-    #         other_args=prefill_args,
-    #     )
-    #
-    # @classmethod
-    # def start_decode(cls):
-    #     decode_args = [
-    #         "--disaggregation-mode",
-    #         "decode",
-    #         "--base-gpu-id",
-    #         2,
-    #         "--tp-size",
-    #         "2",
-    #         "--enable-dp-attention",
-    #         "--dp",
-    #         "2",
-    #         "--load-balance-method",
-    #         cls.decode_load_balance_method,
-    #         "--disaggregation-transfer-backend",
-    #         "ascend",
-    #         "--disable-cuda-graph",
-    #         "--attention-backend",
-    #         "ascend",
-    #         "--mem-fraction-static",
-    #         0.8,
-    #         "--dist-init-addr",
-    #         "127.0.0.1:10000",
-    #     ]
-    #
-    #     cls.process_decode = popen_launch_pd_server(
-    #         cls.model,
-    #         cls.decode_url,
-    #         timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-    #         other_args=decode_args,
-    #     )
-
-
-    def start_prefill(self):
+    @classmethod
+    def start_prefill(cls):
         prefill_args = [
             "--disaggregation-mode",
             "prefill",
@@ -125,7 +66,7 @@ class TestNPULoadBalanceMethodDPDisaggregation(TestDisaggregationBase):
             "--dp",
             "2",
             "--load-balance-method",
-            self.prefill_load_balance_method,
+            cls.prefill_load_balance_method,
             "--disaggregation-transfer-backend",
             "ascend",
             "--disable-cuda-graph",
@@ -139,14 +80,15 @@ class TestNPULoadBalanceMethodDPDisaggregation(TestDisaggregationBase):
             4,
         ]
 
-        self.process_prefill = popen_launch_pd_server(
-            self.model,
-            self.prefill_url,
+        cls.process_prefill = popen_launch_pd_server(
+            cls.model,
+            cls.prefill_url,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             other_args=prefill_args,
         )
 
-    def start_decode(self):
+    @classmethod
+    def start_decode(cls):
         decode_args = [
             "--disaggregation-mode",
             "decode",
@@ -158,7 +100,7 @@ class TestNPULoadBalanceMethodDPDisaggregation(TestDisaggregationBase):
             "--dp",
             "2",
             "--load-balance-method",
-            self.decode_load_balance_method,
+            cls.decode_load_balance_method,
             "--disaggregation-transfer-backend",
             "ascend",
             "--disable-cuda-graph",
@@ -170,52 +112,13 @@ class TestNPULoadBalanceMethodDPDisaggregation(TestDisaggregationBase):
             "127.0.0.1:10000",
         ]
 
-        self.process_decode = popen_launch_pd_server(
-            self.model,
-            self.decode_url,
+        cls.process_decode = popen_launch_pd_server(
+            cls.model,
+            cls.decode_url,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             other_args=decode_args,
         )
 
-        def launch_lb(self):
-            lb_command = [
-                "python3",
-                "-m",
-                "sglang_router.launch_router",
-                "--pd-disaggregation",
-                "--mini-lb",
-                "--prefill",
-                self.prefill_url,
-                "--decode",
-                self.decode_url,
-                "--host",
-                self.base_host,
-                "--port",
-                self.lb_port,
-            ]
-            print("Starting load balancer:", " ".join(lb_command))
-            self.process_lb = popen_with_error_check(lb_command)
-            self.wait_server_ready(self.lb_url + "/health")
-
-    def test_load_balance_method(self):
-        option_list = ["auto", "round_robin", "total_requests", "total_tokens" "follow_bootstrap_room"]
-
-        for self.prefill_load_balance_method, self.decode_load_balance_method in list(itertools.product(option_list, repeat=2)):
-            try:
-                # Non blocking start servers
-                self.start_prefill()
-                self.start_decode()
-
-                # Block until both
-                self.wait_server_ready(self.prefill_url + "/health")
-                self.wait_server_ready(self.decode_url + "/health")
-
-                self.launch_lb()
-
-                self._test_server_info()
-                self._test_gsm8k()
-            finally:
-                super().tearDownClass()
 
     def _test_gsm8k(self):
         args = SimpleNamespace(
@@ -245,6 +148,22 @@ class TestNPULoadBalanceMethodDPDisaggregation(TestDisaggregationBase):
     def tearDownClass(cls):
         os.environ.pop("ASCEND_MF_STORE_URL")
         super().tearDownClass()
+
+
+def create_test_class(param_tuple, index):
+    prefill_load_balance_method, decode_load_balance_method = param_tuple
+    class_name = f"Test_{index:02d}_prefill_{prefill_load_balance_method}_decode_{decode_load_balance_method}"
+    new_class = type(
+        class_name,
+        (BaseTestNPULoadBalanceMethodDPDisaggregation, ),
+        {"params": param_tuple}
+    )
+    return new_class
+
+for index, param_tuple in enumerate(all_params):
+    create_test_class(param_tuple, index)
+
+
 #
 #
 # class _TestNPULoadBalanceMethodPrefillAutoDecodeRoundRobin(TestNPULoadBalanceMethodDPDisaggregation):
