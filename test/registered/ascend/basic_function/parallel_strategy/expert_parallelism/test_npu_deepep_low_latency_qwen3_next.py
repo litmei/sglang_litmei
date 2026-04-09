ba6333c8b71e5b71e1741a9d1aa79c42537ff25c
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-04-09 10:56:14 +0800

    debug

--- test/registered/ascend/basic_function/parallel_strategy/expert_parallelism/test_npu_deepep_low_latency_qwen3_next.py
+++ test/registered/ascend/basic_function/parallel_strategy/expert_parallelism/test_npu_deepep_low_latency_qwen3_next.py
@@ -39,7 +39,7 @@ class TestQwen3Next(CustomTestCase):
         cls.process = popen_launch_server(
             cls.model,
             cls.base_url,
-            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH * 5,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             other_args=[
                 "--trust-remote-code",
                 "--attention-backend",
