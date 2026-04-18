cb849de8b0092c5d48f29afd8c02e7bdda7eb68e
shun8686 <verve@vip.qq.com>
2026-04-18 15:49:41 +0800

    Update npu workflow.

--- python/sglang/test/ascend/e2e/run_aisbench.sh
+++ python/sglang/test/ascend/e2e/run_aisbench.sh
@@ -243,7 +243,7 @@ gsm8k_datasets = [
     dict(
         abbr='gsm8k',
         type=GSM8KDataset,
-        path=$DATASET_PATH,
+        path="$DATASET_PATH",
         reader_cfg=gsm8k_reader_cfg,
         infer_cfg=gsm8k_infer_cfg,
         eval_cfg=gsm8k_eval_cfg)
