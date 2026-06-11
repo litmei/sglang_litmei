049be0a9ec2330c591aefe66503bd58cef9da7c7
YZY00Raiser <2452658175@qq.com>
2026-06-11 14:16:17 +0800

    add

--- test/registered/ascend/basic_function/pd_disaggregation/test_npu_disaggregation_basic.py
+++ test/registered/ascend/basic_function/pd_disaggregation/test_npu_disaggregation_basic.py
@@ -19,7 +19,7 @@ from sglang.test.test_utils import (
 register_npu_ci(est_time=400, suite="full-2-npu-a3", nightly=True)
 
 
-class TestNPUDisaggregationAccuracy(
+class TestNPUDisaggregation(
     PauseResumeInPlaceMixin, PDDisaggregationServerBase
 ):
     @classmethod
