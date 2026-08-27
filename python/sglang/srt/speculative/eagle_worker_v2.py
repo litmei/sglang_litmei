dbbca0115c7c181e71594c893831db4855620284
xjwei <mccllm@qq.com>
2026-08-27 20:29:39 +0800

    Revert "fix"
    This reverts commit 9792991c631e63619872680229132713790ec46d.

--- python/sglang/srt/speculative/eagle_worker_v2.py
+++ python/sglang/srt/speculative/eagle_worker_v2.py
@@ -841,10 +841,7 @@ class EagleDraftWorker(EagleDraftWorkerBase):
                 batch.seq_lens = new_seq_lens
                 if not get_spec().skip_spec_v2_zero_bubble_seq_lens_cpu_sync:
                     batch.seq_lens_cpu = new_seq_lens.to("cpu")
-                    batch.seq_lens_sum = int(batch.seq_lens_cpu.sum())
-                else:
-                    batch.seq_lens_cpu = None
-                    batch.seq_lens_sum = None
+                batch.seq_lens_sum = int(batch.seq_lens_cpu.sum())
                 batch.input_ids = torch.zeros(bs, dtype=torch.int64, device=batch.device)
                 batch.prefix_lens = None
                 batch.extend_lens = None
